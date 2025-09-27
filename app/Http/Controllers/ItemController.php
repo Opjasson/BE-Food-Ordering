2b663ba37a02db7f37113a18a5dbd8ccd4d8ee18
Opjasson <azmitheusos333@gmail.com>
2025-09-27 11:43:13 +0700

    itemController : done setting  createItem and image management file

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ItemController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-        return response(["msg" => "success"], 200);
+        $request->validate([
+            'name' => 'required|max:100',
+            'price' => 'required|integer',
+            'image' => 'nullable|mimes:png,jpg',
+        ]);
+
+        if ($request->file('image')) {
+            $file = $request->file('image');
+            $fileName = $file->getClientOriginalName();
+            $newName = Carbon::now()->timestamp . '_' . $fileName;
+
+            Storage::disk('public')->putFileAs('items', $file, $newName);
+
+            $data = Item::create([
+                "name" => $request->name,
+                "price" => $request->price,
+                "image" => $newName
+            ]);
+
+            return response()->json(['data' => $data]);
+        }
+
+        $data = Item::create($request->all());
+
+        return response()->json(['data' => $data]);
     }
 }
