d96c3da62e6faf7f5e2aa79cc7827ff68e8e3ef2
Opjasson <azmitheusos333@gmail.com>
2025-09-30 20:12:49 +0700

    order : create orderController store function

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use App\Models\Order;
+use App\Models\OrderDetail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $request->validate([
             'customer_name' => 'required|max:100',
             'table_no' => 'required|max:5',
@@ -22,14 +25,24 @@ class OrderController extends Controller
             $data['status'] = 'ordered';
             $data['total'] = 10000;
             $data['waiterss_id'] = $request->user()->id;
+            $data['items'] = $request->items;
 
             $order = Order::create($data);
+
+            collect($data['items'])->map(function ($item) use ($order) {
+                $foodDrink = Item::where('id', $item)->first();
+                OrderDetail::create([
+                    'order_id' => $order->id,
+                    'item_id' => $item,
+                    'price' => $foodDrink->price
+                ]);
+            });
 
             DB::commit();
         } catch (\Throwable $th) {
             DB::rollBack();
             return response($th);
-        }        
+        }
 
         return response(['data' => $order]);
     }
--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetail extends Model
 {
+    protected $table = 'order_detail';
+
     protected $fillable = [
-        'order_id',
-        'item_id',
-        'price'
+        'order_id', 'item_id', 'price'
     ];
 }
